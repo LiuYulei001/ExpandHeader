6f92b4a48341c53c6b4610502dd950966c8aac0b
LYL <445229678@qq.com>
2016-05-24 13:41:48 +0800

    SPIExpandHeader

---
+++ SPIExpandHeader.framework/Headers/SPIExpandHeader.h
@@ -0,0 +1,19 @@
+//
+//  SPIExpandHeader.h
+//  SPIExpandHeader
+//
+//  Created by SPI-绿能宝 on 16/5/24.
+//  Copyright © 2016年 北京SPI绿能宝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CExpandHeader.h"
+//! Project version number for SPIExpandHeader.
+FOUNDATION_EXPORT double SPIExpandHeaderVersionNumber;
+
+//! Project version string for SPIExpandHeader.
+FOUNDATION_EXPORT const unsigned char SPIExpandHeaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPIExpandHeader/PublicHeader.h>
+
+
